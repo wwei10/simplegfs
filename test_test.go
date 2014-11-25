69676726f912553e826741ad2853f4d1d520a3bc
Yanlei Zhao <breezer00@gmail.com>
2014-11-25 00:39:08 -0800

    Fix TestNewClientId
    TestNewClientId now correctlt tests clientId given appropriate master
    server meta data file.

--- test_test.go
+++ test_test.go
@@ -2,19 +2,43 @@ package simplegfs
 
 import (
   "time"
+  "os"
+  "bufio"
   "testing"
+  "strings"
+  "strconv"
 )
 
 func TestNewClientId(t *testing.T) {
   ms := StartMasterServer(":4444")
   time.Sleep(HeartbeatInterval)
+
+  // Read master server's meta data to determine what the next clientId
+  // is suppose to be.
+  f, err := os.OpenFile("serverMeta:4444", os.O_RDONLY, 0666)
+  if err != nil {
+    t.Error(err)
+  }
+  defer f.Close()
+
+  var cid uint64
+  scanner := bufio.NewScanner(f)
+  for scanner.Scan() {
+    fields := strings.Fields(scanner.Text())
+    if fields[0] == "clientId" {
+      cid, _ = strconv.ParseUint(fields[1], 0, 64)
+      break
+    }
+  }
+
   c0 := NewClient(":4444")
-  if c0.clientId != 0 {
+  if c0.clientId != cid {
     t.Error("c0's client id should start with 0.")
   }
   time.Sleep(HeartbeatInterval)
+  cid ++
   c1 := NewClient(":4444")
-  if c1.clientId != 1 {
+  if c1.clientId != cid {
     t.Error("c1's client id should be 1.")
   }
   time.Sleep(HeartbeatInterval)
