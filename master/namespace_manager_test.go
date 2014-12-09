d8b0d0de24ee9d8fbc1476610b92512b0d0772dc
Wei Wei <wwei2@outlook.com>
2014-12-08 22:47:10 -0800

    Modify tests associated with namespace manager

--- master/namespace_manager_test.go
+++ master/namespace_manager_test.go
@@ -31,16 +31,48 @@ func TestGetParent(t *testing.T) {
   assertEquals(t, getParent("/a/b/c"), "/a/b")
 }
 
+func create(m *NamespaceManager, path string) bool {
+  ok, err := m.Create(path)
+  if err != nil {
+    fmt.Println(err)
+  }
+  return ok
+}
+
+func mkdir(m *NamespaceManager, path string) bool {
+  ok, err := m.Mkdir(path)
+  if err != nil {
+    fmt.Println(err)
+  }
+  return ok
+}
+
+func remove(m *NamespaceManager, path string) bool {
+  ok, err := m.Delete(path)
+  if err != nil {
+    fmt.Println(err)
+  }
+  return ok
+}
+
+func list(m *NamespaceManager, path string) []string {
+  ret, err := m.List(path)
+  if err != nil {
+    fmt.Println(err)
+  }
+  return ret
+}
+
 func TestCreateAndMkdir(t *testing.T) {
   m := NewNamespaceManager()
-  assertTrue(t, m.Create("/a"), "Create /a should return true")
-  assertTrue(t, m.Mkdir("/b"), "Mkdir /b should return true")
-  assertTrue(t, m.Mkdir("/b/d"), "Mkdir /b/d should return true")
-  assertTrue(t, m.Mkdir("/b/d/e"), "Create /b/d/e should return true")
-  assertTrue(t, m.Create("/b/c"), "Create /b/c should return true")
-  assertFalse(t, m.Mkdir("/a"), "Mkdir /a should return false")
-  assertFalse(t, m.Create("/a/b"), "Create /a/b should return false") // /a is not a directory
-  assertFalse(t, m.Create("/"), "Create / should return fasle")
+  assertTrue(t, create(m, "/a"), "Create /a should return true")
+  assertTrue(t, mkdir(m, "/b"), "Mkdir /b should return true")
+  assertTrue(t, mkdir(m, "/b/d"), "Mkdir /b/d should return true")
+  assertTrue(t, mkdir(m, "/b/d/e"), "Create /b/d/e should return true")
+  assertTrue(t, create(m, "/b/c"), "Create /b/c should return true")
+  assertFalse(t, mkdir(m, "/a"), "Mkdir /a should return false")
+  assertFalse(t, create(m, "/a/b"), "Create /a/b should return false") // /a is not a directory
+  assertFalse(t, create(m, "/"), "Create / should return fasle")
 }
 
 func TestList(t *testing.T) {
@@ -53,9 +85,9 @@ func TestList(t *testing.T) {
   m.Mkdir("/a/c")
   m.Mkdir("/a/a/b")
   m.Mkdir("/a/a/c")
-  fmt.Println("List /a", m.List("/a"))
-  fmt.Println("List /a/a", m.List("/a/a"))
-  fmt.Println("List /", m.List("/"))
+  fmt.Println("List /a", list(m, "/a"))
+  fmt.Println("List /a/a", list(m, "/a/a"))
+  fmt.Println("List /", list(m, "/"))
 }
 
 func TestDelete(t *testing.T) {
@@ -63,18 +95,18 @@ func TestDelete(t *testing.T) {
   m.Mkdir("/a")
   m.Mkdir("/a/b")
   m.Mkdir("/a/c")
-  assertFalse(t, m.Delete("/a"), "Delete /a should return false")
-  assertTrue(t, m.Delete("/a/b"), "Delete /a/b should return true")
-  assertTrue(t, m.Delete("/a/c"), "Delete /a/c should return true")
-  assertTrue(t, m.Delete("/a"), "Delete /a should return true")
+  assertFalse(t, remove(m, "/a"), "Delete /a should return false")
+  assertTrue(t, remove(m, "/a/b"), "Delete /a/b should return true")
+  assertTrue(t, remove(m, "/a/c"), "Delete /a/c should return true")
+  assertTrue(t, remove(m, "/a"), "Delete /a should return true")
   m.Mkdir("/a")
   m.Mkdir("/b")
   m.Mkdir("/b/c")
   m.Mkdir("/b/c/d")
-  assertFalse(t, m.Delete("/b/c"), "Delete /b/c should return false")
-  assertTrue(t, m.Delete("/b/c/d"), "Delete /b/c/d should return true")
-  assertTrue(t, m.Delete("/b/c"), "Delete /b/c should return true")
-  assertTrue(t, m.Delete("/b"), "Delete /b should return true")
+  assertFalse(t, remove(m, "/b/c"), "Delete /b/c should return false")
+  assertTrue(t, remove(m, "/b/c/d"), "Delete /b/c/d should return true")
+  assertTrue(t, remove(m, "/b/c"), "Delete /b/c should return true")
+  assertTrue(t, remove(m, "/b"), "Delete /b should return true")
 }
 
 func TestSaveAndLoad(t *testing.T) {
@@ -92,8 +124,8 @@ func TestSaveAndLoad(t *testing.T) {
   // some directories because they already exist in the namespace.
   m1 := NewNamespaceManager()
   m1.Load(path)
-  assertFalse(t, m1.Mkdir("/a"), "m1 Mkdir /a should fail.")
-  assertFalse(t, m1.Mkdir("/a/b"), "m1 Mkdir /a/b should fail." )
-  assertFalse(t, m1.Mkdir("/a/c"), "m1 Mkdir /a/c should fail.")
-  assertTrue(t, m1.Mkdir("/a/b/c"), "m1 Mkdir /a/b/c should succeed.")
+  assertFalse(t, mkdir(m1, "/a"), "m1 Mkdir /a should fail.")
+  assertFalse(t, mkdir(m1, "/a/b"), "m1 Mkdir /a/b should fail." )
+  assertFalse(t, mkdir(m1, "/a/c"), "m1 Mkdir /a/c should fail.")
+  assertTrue(t, mkdir(m1, "/a/b/c"), "m1 Mkdir /a/b/c should succeed.")
 }
